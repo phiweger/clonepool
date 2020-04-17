d400a9b77ee3e28c032953b22e9d7f00c1b6c9bf
Felix Kuehnl <felix@bioinf.uni-leipzig.de>
2020-04-17 20:07:45 +0200

    Added pytest and tox deps to conda env and setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
+        'tox',
+        'pytest',
         'click',
         'networkx',
         'numpy',
